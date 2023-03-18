aa7f391458e408f2a5123743829827ff842833f7
abdulah-khaleel <abdulah.khaleel@gmail.com>
2023-03-18 15:46:55 +0500

    [Add] exercise 3.9

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const cors = require('cors')
 
 const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 morgan.token('rqBody', function (req, res) {
   const reqBody = JSON.stringify(req.body)

44db67962269117bee47ff25cb3e687a09380c23
abdulah-khaleel <abdulah.khaleel@gmail.com>
2023-03-20 00:02:23 +0500

    [Add] exercise 3.12 complete

--- mongo.js
+++ mongo.js
@@ -4,19 +4,27 @@ const password = process.argv[2]
 const name = process.argv[3]
 const number = process.argv[4]
 
-const url = `mongodb+srv://fullstack:${password}@cluster0.bnkizsw.mongodb.net/noteApp?retryWrites=true&w=majority`
+const url = `mongodb+srv://fullstack:${password}@cluster0.bnkizsw.mongodb.net/phoneBookApp?retryWrites=true&w=majority`
 
 mongoose.connect(url)
 
-run()
-async function run() {
-  const db = mongoose.connection
+if (process.argv.length < 3) {
+  console.log(
+    'give password as argument. add name and number for saving person.'
+  )
+  process.exit(1)
+} else if (process.argv.length === 3) {
+  run('list')
+} else {
+  run('add')
+}
 
+async function run(action) {
+  const db = mongoose.connection
   db.on('error', console.error.bind(console, 'connection error:'))
 
   db.once('open', function () {
-    console.log('Connection Successful!')
-
+    // console.log('Connection Successful!')
     const personSchema = new mongoose.Schema({
       name: String,
       number: String,
@@ -24,13 +32,23 @@ async function run() {
 
     const Person = mongoose.model('Person', personSchema)
 
-    const person = new Person({
-      name: name,
-      number: number,
-    })
-    person.save().then((result) => {
-      console.log('person saved!')
-      mongoose.connection.close()
-    })
+    if (action === 'list') {
+      console.log('phonebook')
+      Person.find({}).then((result) => {
+        result.forEach((p) => {
+          console.log(`${p.name} ${p.number}`)
+        })
+        mongoose.connection.close()
+      })
+    } else if (action === 'add') {
+      const person = new Person({
+        name: name,
+        number: number,
+      })
+      person.save().then((result) => {
+        console.log('person saved!')
+        mongoose.connection.close()
+      })
+    }
   })
 }

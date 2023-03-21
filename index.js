1f5f03bcd6ff4d4e7693a80d6491e25ad63a98a1
abdulah-khaleel <abdulah.khaleel@gmail.com>
2023-03-21 18:47:40 +0500

    [Add] exercise 3.16

--- index.js
+++ index.js
@@ -82,12 +82,13 @@ app.get('/api/persons', (request, response) => {
 app.get('/api/persons/:id', (request, response) => {
   Person.findById(request.params.id)
     .then((person) => {
-      response.json(person)
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
     })
-    .catch((error) => {
-      console.log(error)
-      response.status(400).send({ error: 'malformatted id' })
-    })
+    .catch((error) => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {

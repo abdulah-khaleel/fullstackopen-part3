6349903756a7a225301622a70b45832a33cb825f
abdulah-khaleel <abdulah.khaleel@gmail.com>
2023-03-21 18:18:13 +0500

    [Add] exercise 3.15

--- index.js
+++ index.js
@@ -15,13 +15,24 @@ morgan.token('rqBody', function (req, res) {
   return ' '
 })
 app.use(morgan(':method :url :status - :response-time ms  :rqBody'))
-app.use(express.json())
-app.use(cors())
-app.use(express.static('build'))
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
+
+app.use(express.json())
+app.use(cors())
+app.use(express.static('build'))
 
 let persons = []
 
@@ -73,19 +84,22 @@ app.get('/api/persons/:id', (request, response) => {
     .then((person) => {
       response.json(person)
     })
-    .catch(() => {
-      response.status(404).end()
+    .catch((error) => {
+      console.log(error)
+      response.status(400).send({ error: 'malformatted id' })
     })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter((p) => p.id !== id)
-
-  response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then((result) => {
+      response.status(204).end()
+    })
+    .catch((error) => next(error))
 })
 
 app.use(unknownEndpoint)
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 
